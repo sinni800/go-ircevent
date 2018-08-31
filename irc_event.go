901c707cf0399c0b47a797fc7d56dc9967b4ca60
sinni800 <sinni800@users.noreply.github.com>
2018-08-31 16:25:04 +0200

    Create irc_event.go

---
+++ irc_event.go
@@ -0,0 +1,22 @@
+package irc
+
+import (
+	"strings"
+)
+
+func (e *IRCEvent) IsChannelMessage() bool {
+	if strings.HasPrefix(e.Arguments[0], "#") {
+		return true
+	}
+	return false
+}
+
+func (e *IRCEvent) GetMessageSource() string {
+	if len(e.Arguments) == 0 {
+		return e.Message
+	}
+	if strings.HasPrefix(e.Arguments[0], "#") {
+		return e.Arguments[0]
+	}
+	return e.Nick
+}
