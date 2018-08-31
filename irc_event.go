f9919e55ee0702e05a0777d0649117820f9cc9c5
sinni800 <sinni800@users.noreply.github.com>
2018-08-31 16:46:07 +0200

    Update irc_event.go

--- irc_event.go
+++ irc_event.go
@@ -4,14 +4,14 @@ import (
 	"strings"
 )
 
-func (e *IRCEvent) IsChannelMessage() bool {
+func (e *Event) IsChannelMessage() bool {
 	if strings.HasPrefix(e.Arguments[0], "#") {
 		return true
 	}
 	return false
 }
 
-func (e *IRCEvent) GetMessageSource() string {
+func (e *Event) GetMessageSource() string {
 	if len(e.Arguments) == 0 {
 		return e.Message()
 	}
